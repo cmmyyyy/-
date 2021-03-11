642b5109a982a2c5cefe4589629735716aa59d1a
Magnesium <72329596+cmmyyyy@users.noreply.github.com>
2021-03-11 10:26:48 +0800

    Add files via upload

---
+++ 线性结构/线性结构5/源.cpp
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <assert.h>
+#include "Node.h"
+
+
+int main()
+{
+	struct List list = create();
+	struct Node* s;
+	int m, n, i;
+	scanf("%d %d", &m, &n);
+	for (i = 1;i <= m;i++)
+	{
+		list = NodeInsert(list, i);
+	}
+	s = list.head;
+	list.tail->next = list.head->next;
+	while (list.head->next->data != list.tail->data)
+	{
+		i = n;
+		while (--i)
+		{
+			s = s->next;
+		}
+		printf("%d ", s->next->data);
+		if (s->next == list.tail)
+		{
+			list.tail = list.tail->next;
+		}
+		struct Node* p = s->next;
+		s->next = s->next->next;
+		free(p);
+	}
+	
+	Destroy(list);
+	return 0;
+}
